e5601c93d615e52d5199a13e6d2f940f0e9a5840
Tom Hall <thattommyhall@gmail.com>
2020-09-03 16:47:10 +0200

    make the periodic refresh work

--- conf.d/lua/init_worker.lua
+++ conf.d/lua/init_worker.lua
@@ -1,12 +1,13 @@
-local ngx = ngx
-local http = require "resty.http"
 local helpers = require "helpers"
 
-local function refresh(httpc, hostname)
+local function refresh_acl()
+    ngx.log(ngx.INFO, "REFRESHING")
+    local client = helpers.apiclient("http://app:5000")
+    local acl = client.get("/acl")
+    if client and acl then
+        helpers.update_acl(acl)
+    end
 end
 
-local function refresh_all()
-end
-
--- ngx.timer.at(0, refresh_all)
-ngx.timer.every(15, refresh_all)
+ngx.timer.at(0, refresh_acl)
+ngx.timer.every(10, refresh_acl)
